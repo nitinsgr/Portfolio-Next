dc9c92c0e9c4af3480d4904a7b448805a7e1b199
Nitin sagar <ns85596@gmail.com>
2021-12-06 08:45:25 -0800

    Modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: {
-    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
+    content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"]
     // These options are passed through directly to PurgeCSS
   },
   darkMode: false, // or 'media' or 'class'
